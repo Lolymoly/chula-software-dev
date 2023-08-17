af5e505680bd577577bd8c5be8cecb429e1f7abb
Lolymoly <teetat.wis@gmail.com>
2023-08-17 11:38:21 +0700

    seperate each div animation

--- script/animation.js
+++ script/animation.js
@@ -1,18 +1,21 @@
-const descriptionDivs = document.querySelectorAll(".description-div");
+const descriptionDivs = document.querySelectorAll(".number-and-desc");
 
 const observerOptions = {
   threshold: 0.5, // Adjust the threshold as needed
 };
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.style.animation = "none"; // Reset animation
-      void entry.target.offsetWidth; // Trigger reflow
-      entry.target.style.animation = "slideIn 1s forwards";
-      observer.unobserve(entry.target);
-    }
+const animateDivsWithDelay = (divs) => {
+  divs.forEach((div, index) => {
+    setTimeout(() => {
+      div.classList.add("animate-slide");
+      observer.unobserve(div);
+    }, index * 200); // Apply a delay per div
   });
+};
+
+const observer = new IntersectionObserver((entries) => {
+  const intersectingDivs = entries.filter((entry) => entry.isIntersecting);
+  animateDivsWithDelay(intersectingDivs.map((entry) => entry.target));
 }, observerOptions);
 
 descriptionDivs.forEach((div) => {
